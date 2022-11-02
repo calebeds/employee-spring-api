b014f1d00420b162257061395322be91cad0428e
Calebe Dos Santos Oliveira <calebe.dso@gmail.com>
2022-11-02 16:20:53 -0300

    add service interface

--- src/main/java/com/calebe/apiapplication/service/EmployeeService.java
+++ src/main/java/com/calebe/apiapplication/service/EmployeeService.java
@@ -1,13 +1,22 @@
 package com.calebe.apiapplication.service;
 
-public class EmployeeService {
+import com.calebe.apiapplication.domain.Employee;
+
+import java.util.List;
+
+public interface EmployeeService {
     // Save an employee
+    void addEmployee(Employee employee);
 
     // Get employees
+    List<Employee> getAllEmployees();
 
     // Get one employee
+    Employee findById(Integer id);
 
     // Update an employee
+    void updateEmployee(Employee employee);
 
     // Delete employee
+    Boolean deleteById(Integer id);
 }
